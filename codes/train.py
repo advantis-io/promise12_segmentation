8b7a6de313b4afa7dc8574789e48a8b0cd0cbc44
Martin Stypinski <mstypinski@gmail.com>
2018-10-05 09:39:46 +0200

    reduced batch size

--- codes/train.py
+++ codes/train.py
@@ -231,7 +231,7 @@ if __name__=='__main__':
 
     start = time.time()
     keras_fit_generator(img_rows=256, img_cols=256, regenerate=False,
-                        n_imgs=10**4, batch_size=32)
+                        n_imgs=10**4, batch_size=16)
 
     end = time.time()
 
